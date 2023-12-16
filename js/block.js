04b6c47d4d9fe60bd53c8c5e8c86dbe1a70de39f
Wederson Cabral Domingues <wederson.domingues@stz.com.br>
2023-12-16 16:34:09 -0300

    Change the form of generation the blocks to hundle the to HTML

--- js/block.js
+++ js/block.js
@@ -1,98 +1,107 @@
-const BLOCKS ={}
-BLOCKS.BLOCKS_LIST = []
+const BLOCKS = {
+    block_list: []
+}
+BLOCKS.add_block = (block)=>BLOCKS.block_list.push(block);
+BLOCKS.get_blocks = ()=>BLOCKS.block_list;   
+BLOCKS.make_it_draggable = (element) => new PlainDraggable(element);
+BLOCKS.get_block = (blockId) =>  BLOCKS.block_list.find(block => block.id === blockId);
+BLOCKS.get_blocks_rendered = () => BLOCKS.block_list.find(block => block.status === 'rendered');
 
-BLOCKS.make_it_draggable=(element)=>{
-    return new PlainDraggable(element);
- }
 
-BLOCKS.delete_stream_block_start = ()=>{
+BLOCKS.delete_stream_block_start = () => {
     const startBlock = document.getElementsByClassName('stream-block-start')[0]
-    startBlock.style.display='none'
+    startBlock.style.display = 'none'
 }
 
-BLOCKS.render_stream_block_start = ()=>{
+BLOCKS.render_stream_block_start = () => {
     const startBlock = document.getElementsByClassName('stream-block-start')[0]
-    startBlock.style.display='flex'
+    startBlock.style.display = 'flex'
 }
 
-BLOCKS.get_block_template =async (blockTempalteName,format)=>{
+BLOCKS.get_block_template = async (blockTempalteName, format) => {
 
-    format=format || 'text'
+    format = format || 'text'
 
-    rawHtml=await fetch('./blocks/'+blockTempalteName+'.html',{mode: "no-cors",credentials: "same-origin"}).then(response=>response.text())
+    rawHtml = await fetch('./blocks/' + blockTempalteName + '.html', { mode: "no-cors", credentials: "same-origin" }).then(response => response.text())
 
-    if(format==='text'){
+    if (format === 'text') {
         return rawHtml
     }
 
-    if(format==='html'){
+    if (format === 'html') {
         return UTILS.text_to_html(rawHtml)
     }
 
 }
-BLOCKS.get_block=(blockId)=>{
 
-    return BLOCKS.BLOCKS_LIST.find(block => block.id === blockId);
-}
-BLOCKS.get_blocks_rendered=()=>{
-
-    return BLOCKS.BLOCKS_LIST.find(block => block.status === 'rendered');
+BLOCKS.render_block_miniature = (block)=>{
+    const blockViewSection = document.getElementsByClassName('content-blockView')[0];
+    blockViewSection.appendChild(block.html.miniature)
+    BLOCKS.make_it_draggable(block.html.miniature)
 }
 
+BLOCKS.render_block_setup = (block) => {
 
-BLOCKS.render_block=(block)=>{
-
-    if(BLOCKS.BLOCKS_LIST.length=1){
+    if (BLOCKS.block_list.length == 1) {
         BLOCKS.delete_stream_block_start()
     }
 
-    if(typeof(block)==='string'){
-        block=BLOCKS.get_block(block)
+    if (typeof (block) === 'string') {
+        block = BLOCKS.get_block(block)
     }
 
     const streamSection = document.getElementsByClassName('content-blockSetup')[0];
-    streamSection.appendChild(block.html)
-    block.status='rendered'
-    if(BLOCKS.BLOCKS_LIST.length>1){
+    streamSection.appendChild(block.html.setup)
+
+    if (BLOCKS.block_list.length > 1) {
         BLOCKS.delete_stream_block_start()
     }
 
 }
 
-BLOCKS.create_block_empty=async ()=>{
-    
-    const block_new_html= await BLOCKS.get_block_template('stream_block_empty','html');
+BLOCKS.create_block_empty = async () => {
 
-    const block_empty ={
-        id: Date.now().toString()+'-'+Math.random().toString(16).slice(2),
+    const block_empty = {
+        id: Date.now().toString() + '-' + Math.random().toString(16).slice(2),
         type: '',
-        html: block_new_html,
+        html: {},
         status: 'created',
+        name: ''
     }
-
-    BLOCKS.BLOCKS_LIST.push[block_empty]
 
     return block_empty
 
 }
 
+BLOCKS.create_stream_block_code_miniature = async () => await BLOCKS.get_block_template('stream_block_code_miniature', 'html');
 
-BLOCKS.create_new_stream_block_code=async ()=>{
+BLOCKS.create_stream_block_code_setup = async () => {
 
-    const block_object=  await BLOCKS.create_block_empty()
-    block_object.type='code'
-
-    const block_code_html=   await BLOCKS.get_block_template('stream_block_code_setup','html')
+    const block_code_html = await BLOCKS.get_block_template('stream_block_code_setup', 'html')
     const block_code_body = block_code_html.querySelector('.stream-block-code-body')
 
     ace.edit(block_code_body)
-    console.log(block_code_html)
-    block_object.html.appendChild(block_code_html)
 
-    return block_object
+    return block_code_html
+
 }
 
-BLOCKS.render_new_block_code=async()=>{
-    block_code=await BLOCKS.create_new_stream_block_code();
-    BLOCKS.render_block(block_code)
+BLOCKS.create_stream_block_code= async () =>{
+    const block_object = await BLOCKS.create_block_empty()
+    block_object.type = 'code'
+    block_object.html.setup= await BLOCKS.create_stream_block_code_setup()
+    block_object.html.miniature = await BLOCKS.create_stream_block_code_miniature()
+
+    return block_object;
+}
+
+
+
+BLOCKS.render_new_block_code = async () => {
+    const block_code = await BLOCKS.create_stream_block_code();
+    BLOCKS.render_block_setup(block_code)
+    BLOCKS.render_block_miniature(block_code)
+    block_code.status = 'rendered'
+    BLOCKS.add_block(block_code)
+
 }
